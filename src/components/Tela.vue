17ea73c647769480243ada412c4afb63711df648
FIAP <FIAP>
2019-11-28 22:11:59 -0300

    Instalamos axios para fazer requisição de api e estilizamos o retorno; criamos um filtro; utilizamos web componente; criação de busca com ajuda de filtre e RegExp;

--- src/components/Tela.vue
+++ src/components/Tela.vue
@@ -1,17 +1,24 @@
 <template>
-    <div>
-        <h1>{{msg}}</h1>
-        <h3 v-text="titulo + ' olá'"></h3>
-        <input v-model="message">
-        <button v-on:click="reverseMessage">Inverter</button>
-        <p>{{message}}</p>
-        <p v-text="titulo + new Date().toLocaleString()"></p>
-        
-        <span v-if="seen">Mostra Isso</span>
-        <img :src="foto.url" :alt="foto.alt">
-        <ul>
-            <li v-for="foto in fotos" :key="foto.id">
-                <img :src="foto.url" :alt="foto.alt" width="188" height="125">
+    <div class="corpo">
+        <h1 class="centralizado">{{msg}}</h1>
+        <input
+            type="search"
+            placeholder="Filtre pelo nome da foto"
+            class="filtro"
+            @input="filtro = $event.target.value"
+        />
+        <ul class="lista-fotos">
+            <li v-for="user in fotosComFiltro" :key="user.id" class="lista-fotos-item">
+                <meu-painel :titulo="user.first_name + ' ' +  user.last_name">
+                    <img
+                        class="imagem-responsiva"
+                        :src="user.avatar"
+                        :alt="user.first_name + ' ' + user.last_name"
+                        :title="user.first_name + ' ' +  user.last_name"
+                        width="187px"
+                        height="128px"
+                    />
+                </meu-painel>
             </li>
         </ul>
     </div>
@@ -19,21 +26,51 @@
 </template>
 
 <script>
+
+import axios from "axios";
+import Painel from "./shared/Painel"
 export default {
+    computed: { // olha as alteracoes que houveram dentro do data()
+        fotosComFiltro() {
+            if(this.filtro) {
+                let expressaoRegular = new RegExp(this.filtro.trim(), "i");
+                return this.users.filter((user) => expressaoRegular.test(user.first_name));
+            } else {
+                return this.users;
+            }
+        }
+    },
+    components: {
+        "meu-painel": Painel
+    },
     name: "Tela",
     props: {
-        msg: String
-    }, 
+        msg: String,
+    },
     methods: {
         reverseMessage(){
             this.message = this.message.split('').reverse().join('')
         }
     },
+    created() {
+        this.loading = true;
+        axios
+            .get("https://reqres.in/api/users")
+            .then(response => {
+                this.loading = false;
+                this.users = response.data.data;
+            }).catch(error => {
+                this.loading = false;
+                this.errors.push(error);
+            })
+    },
+
     data(){
         return {
             message: "Texto a ser invertido: romano acata amores a damas amadas e Roma ataca o namoro",
             titulo : "Titulo da minha página",
             seen: false,
+            loading: false,
             foto: {
                 url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
                 alt: "Cachorro"
@@ -54,8 +91,10 @@ export default {
                     url: "https://images.pexels.com/photos/35992/gorilla-monkey-ape-zoo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260",
                     alt: "Gorilla"
                 }
-                
-            ]
+            ],
+            users:[],
+            errors: [],
+            filtro: ''
         }
     },
 }
@@ -63,5 +102,34 @@ export default {
 
 
 <style scoped>
+.filtro {
+    display: block;
+    width: 100%;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, Arial, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+  color: gray;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+/* estilo do painel */
+
+.imagem-responsiva {
+    width: 100%;
+}
 
 </style>
